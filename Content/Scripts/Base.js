06b5e7382958268dd15bcd6712451c75852df4ac
Can Ceylandağ <69530509+canceylandag@users.noreply.github.com>
2022-12-16 22:37:36 +0300

    3. Madde Yarım

--- Content/Scripts/Base.js
+++ Content/Scripts/Base.js
@@ -1,13 +1,15 @@
 Filter={
     Apis:{
         fetchCategoryUrl:"https://dummyjson.com/products/categories",
+        fetchAllItems:"https://dummyjson.com/products?limit=100",
+        fetchSearch:"https://dummyjson.com/products/search?limit=100&q=",
     },
     Elements:{
         categoryList:document.getElementById("category-list"),
         productList:document.getElementById("product-list"),
         cardTemp:document.getElementById("card-temp"),
         categoryTemp:document.getElementById("category-list-item-temp"),
-        
+    
     },
     Status:{
         categories:[],
@@ -18,6 +20,7 @@ Filter={
     Actions:{
         init:()=>{
             Filter.Actions.getAllCategories();
+            Filter.Actions.getAllItems();
         },
         appendCategoriesToHtml:()=>{
             Filter.Elements.categoryList.innerHTML="";
@@ -31,7 +34,7 @@ Filter={
                 var text=ctg.replaceAll("-"," ");
                 var firstChar=text.charAt(0).toLocaleUpperCase();
                 text=firstChar+text.substring(1);
-
+                
                 div.querySelector("label").innerText=text;
                 Filter.Elements.categoryList.appendChild(div.querySelector("li"));
             }
@@ -49,7 +52,69 @@ Filter={
         },
         clear:()=>{
             
-        },    
+        },
+        getAllItems:()=>{
+            fetch(Filter.Apis.fetchAllItems)
+            .then(res=>res.json())
+            .then(res=>{
+                Filter.Actions.appendListToHtml(res.products);
+            })
+
+
+
+        },
+        searchList:()=>{
+            var searchParam=document.getElementById("search-input").value;
+            fetch(Filter.Apis.fetchSearch+searchParam)
+            .then(res=>res.json())
+            .then(res=>{
+                Filter.Actions.appendListToHtml(res.products);
+            })
+
+        },
+        searchCategory:()=>{
+            
+            var searchParam=document.getElementById("search-category").value; 
+            var cloneArray=[...Filter.Status.categories];
+           
+
+            if(searchParam!=""){
+                function _filter(data){
+                    return data.includes(searchParam);
+                }
+                cloneArray=cloneArray.filter(x=>x.toLocaleLowerCase().includes(searchParam));
+
+                for (let i = 0; i <Filter.Status.categories.length; i++) {
+                    
+                    if( cloneArray.includes(Filter.Status.categories[i]) ){
+
+                        Filter.Elements.categoryList.querySelector("#"+Filter.Status.categories[i]).parentElement.style.display="block";
+                        Filter.Elements.categoryList.querySelector("#"+Filter.Status.categories[i]).checked=false;
+                    }else{
+                        Filter.Elements.categoryList.querySelector("#"+Filter.Status.categories[i]).parentElement.style.display="none";
+                    }
+                    
+                }
+
+            }else{
+                Filter.Actions.getAllCategories();
+            }
+            
+            
+        },
+        appendListToHtml:(param)=>{
+            Filter.Elements.productList.innerHTML="";
+            for (let i = 0; i < param.length; i++) {
+                const ctg = param[i];
+                var div=document.createElement("div");
+                div.innerHTML=Filter.Elements.cardTemp.innerHTML;
+                div.querySelector("img").setAttribute("src",ctg.images[0]);
+                div.querySelector("h5").innerText=ctg.title; 
+                div.querySelector("p").innerText=ctg.description;
+                Filter.Elements.productList.appendChild(div.querySelector("a"));
+            } 
+        },
+
     }
 
 
